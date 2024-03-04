ffee6cfa751a62361efb2f7687c61e3d92ee028d
S-furi <stefano.furi@studio.unibo.it>
2024-03-04 19:00:30 +0100

    chore: add implementation for Deterministic positioning policy

--- src/main/java/e1/DeterministicPositioningPolicy.java
+++ src/main/java/e1/DeterministicPositioningPolicy.java
@@ -2,15 +2,37 @@ package e1;
 
 public class DeterministicPositioningPolicy implements PositioningPolicy {
 
+    private final int size;
+    private final Pair<Integer, Integer> pawnPosition;
+    private final Pair<Integer, Integer> knightPosition;
+
+    public DeterministicPositioningPolicy(int size, Pair<Integer, Integer> knightPosition, Pair<Integer, Integer> pawnPosition) {
+        this.size = size;
+        checkPositions(knightPosition, pawnPosition);
+        this.knightPosition = knightPosition;
+        this.pawnPosition = pawnPosition;
+    }
+
+    private void checkPositions(final Pair<Integer, Integer> knightPosition, final Pair<Integer, Integer> pawnPosition) {
+        if (knightPosition.equals(pawnPosition)) {
+            throw new IllegalArgumentException("Knight should be placed in a different position than Pawn.");
+        } else if (!isPositionInsideBoundaries(knightPosition) || !isPositionInsideBoundaries(pawnPosition)) {
+            throw new IllegalArgumentException("Positions must be placed inside the " + this.size + "x" + this.size + "grid!");
+        }
+    }
+
+    private boolean isPositionInsideBoundaries(final Pair<Integer, Integer> position) {
+        return (position.getX() >= 0 && position.getX() < this.size)
+                && (position.getY() >= 0 && position.getY() < this.size);
+    }
+
     @Override
     public Pair<Integer, Integer> getKnightPosition() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getKnightPosition'");
+        return this.knightPosition;
     }
 
     @Override
     public Pair<Integer, Integer> getPawnPosition() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPawnPosition'");
+        return this.pawnPosition;
     }
 }
---
+++ src/test/java/e1/DeterministicPositioningPolicyTest.java
@@ -0,0 +1,52 @@
+package e1;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+@TestInstance(Lifecycle.PER_CLASS)
+public class DeterministicPositioningPolicyTest extends PositioningPolicyTest {
+    final Pair<Integer, Integer> knightPosition = new Pair<>(0, 0);
+    final Pair<Integer, Integer> pawnPosition = new Pair<>(1, 2);
+
+    @Override
+    protected PositioningPolicy createPositioningPolicy() {
+        return new DeterministicPositioningPolicy(this.size, this.knightPosition, this.pawnPosition);
+    }
+
+    @Test
+    void testDeterministicPositioningPolicyCreation() {
+        assertNotNull(new DeterministicPositioningPolicy(this.size, this.knightPosition, this.pawnPosition));
+    }
+
+    @Test
+    void testPositioningPolicyWithOverlappingPositionsShouldThrowException() {
+        final Pair<Integer, Integer> pos = new Pair<>(0, 0);
+        assertThrows(IllegalArgumentException.class, () -> new DeterministicPositioningPolicy(this.size, pos, pos));
+    }
+
+    @Test
+    void testPositioningPolicyWithOutOfBoundariesPositionsShouldThrowException() {
+        final Pair<Integer, Integer> pos = new Pair<>(-1, -1);
+
+        assertAll(
+            () -> assertThrows(IllegalArgumentException.class, () -> new DeterministicPositioningPolicy(this.size, pos, this.pawnPosition)),
+            () -> assertThrows(IllegalArgumentException.class, () -> new DeterministicPositioningPolicy(this.size, this.knightPosition, pos))
+        );
+    }
+
+    @Test
+    void testDeterministicKnightPosition() {
+        assertEquals(this.knightPosition, this.positioningPolicy.getKnightPosition());
+    }
+
+    @Test
+    void testDeterministicPawnPosition() {
+        assertEquals(this.pawnPosition, this.positioningPolicy.getPawnPosition());
+    }
+}
