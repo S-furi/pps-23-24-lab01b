0e48b7631962f76cbea5b98b1165045d01d912a2
S-furi <stefano.furi@studio.unibo.it>
2024-03-04 19:00:39 +0100

    refactor: rename test

--- src/test/java/e1/PositioningPolicyTest.java
+++ src/test/java/e1/PositioningPolicyTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 
 public abstract class PositioningPolicyTest {
     protected PositioningPolicy positioningPolicy;
+    protected final int size = 5;
 
     @BeforeAll
     void setUp() {
@@ -17,7 +18,7 @@ public abstract class PositioningPolicyTest {
     protected abstract PositioningPolicy createPositioningPolicy();
 
     @Test
-    void testSomething() {
+    void testPositioningiPolicyShouldHaveNotNullPiecesPositions() {
         assertAll(
             () -> assertNotNull(this.positioningPolicy.getKnightPosition()),
             () -> assertNotNull(this.positioningPolicy.getPawnPosition())
