951ffc001a37574ba0d47ce9a8c7dff63479a99c
S-furi <stefano.furi@studio.unibo.it>
2024-03-06 16:16:02 +0100

    feat: add mine placement logic

--- src/main/java/e2/LogicsImpl.java
+++ src/main/java/e2/LogicsImpl.java
@@ -1,8 +1,39 @@
 package e2;
 
-public class LogicsImpl implements Logics {
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
 
-    public LogicsImpl(int size) {
+public class LogicsImpl implements Logics {
+    final int size;
+    final int numberOfMines;
+    private final List<Pair<Integer, Integer>> mines;
+    private final Random random = new Random();
+
+    public LogicsImpl(final int size, final int numberOfMines) {
+        this.size = size;
+        this.numberOfMines = numberOfMines;
+        this.mines = this.generateMines();
+    }
+    
+    @Override
+    public boolean hasMine(final Pair<Integer, Integer> position) {
+        return this.mines.contains(position);
+    }
+    
+    private List<Pair<Integer, Integer>> generateMines() {
+        final List<Pair<Integer, Integer>> generatedMines = new ArrayList<>();
+        for (int i = 0; i < this.numberOfMines; i++) {
+            Pair<Integer, Integer> minePosition = generateRandomPosition();
+            while (generatedMines.contains(minePosition)) {
+                minePosition = generateRandomPosition();
+            }
+            generatedMines.add(minePosition);
+        }
+        return List.copyOf(generatedMines);
     }
 
+    private Pair<Integer, Integer> generateRandomPosition() {
+        return new Pair<>(this.random.nextInt(this.size), this.random.nextInt(this.size));
+    }
 }
---
+++ src/test/java/e2/LogicsTest.java
@@ -0,0 +1,42 @@
+package e2;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class LogicsTest {
+    private final int size = 7;
+    private final int numberOfMines = 2;
+    private Logics logics;
+
+    @BeforeEach
+    void setUp() {
+        this.logics = new LogicsImpl(this.size, this.numberOfMines);
+    }
+
+    @Test
+    void testLogicCreationShouldNotBeNull() {
+        assertNotNull(new LogicsImpl(this.size, this.numberOfMines));
+    }
+
+    @Test
+    void logicsShouldHaveRightAmountOfMines() {
+        final List<Pair<Integer, Integer>> minesFound = new ArrayList<>();
+
+        for (int i = 0; i < this.size; i++) {
+            for (int j = 0; j < this.size; j++) {
+                final Pair<Integer, Integer> pos = new Pair<Integer, Integer>(i, j);
+                if (logics.hasMine(pos)) {
+                    minesFound.add(pos);
+                }
+            }
+        }
+
+        assertEquals(numberOfMines, minesFound.size());
+    }
+}
